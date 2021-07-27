e54d4e0beb7aba4cc5d75e195486a4738487ff9d
Aleksandar Djordjevic <aleksandars.djordjevic@iwedia.com>
2021-07-27 16:13:29 +0200

    Second commit

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,55 @@
-enum Link{
+enum Link {
     Empty,
     NoneEmpty(Box<Node>),
 }
 
-struct Node{
-    left : Link,
-    right : Link,
-    value : u32
+struct Node {
+    left: Link,
+    right: Link,
+    value: u32,
 }
 
-pub struct BST {
-    nodes : Vec<Node>,
-    item_count : u32
+struct BinarySearchTree {
+    head: Link,
+    item_count: u32,
 }
 
-impl BST{
+impl BinarySearchTree {
+    fn new() -> Self {
+        BinarySearchTree {
+            head: Link::Empty,
+            item_count: 0,
+        }
+    }
 
-    pub fn size(&self) -> u32{
+    fn is_empty(&self) -> bool {
+        self.item_count == 0
+    }
+
+    fn size(&self) -> u32 {
         self.item_count
     }
-}
 
-impl BST{
-    pub fn new() -> Self {
-        BST {
-            item_count : 0,
-            nodes : Vec::new()
+    fn push(&mut self, item : u32){
+        let new_node = Node {
+            left : Link::Empty,
+            right : Link::Empty,
+            value : item,
+        };
+        if self.is_empty() {
+            self.head = Link::NoneEmpty{
+                
+            }
         }
     }
 }
 
 fn main() {
     println!("Hello, world!");
+    let bst = BinarySearchTree::new();
+    if bst.is_empty() {
+        println!("Bst is empty!");
+    } else {
+        println!("Item count {}", bst.size());
+    }
 }
